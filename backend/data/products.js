8b3e73882f95b3c2d4c67b22031c50aeb46770b2
Dano <danielaherniv@gmail.com>
2020-11-01 09:26:44 -0500

    working on Redux store

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,46 +1,46 @@
 const products = [
   {
    
-    name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/airpods.jpg',
+    name: 'Essential Oils',
+    image: '/images/assentialOil.png',
     description:
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 89.99,
+      'Smells very nice.  You can rub it on your face and then bark like a dog.',
+    brand: 'Young Living',
+    category: 'Fragrances',
+    price: 5.99,
     countInStock: 10,
     rating: 4.5,
     numReviews: 12,
   },
   {
 
-    name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
+    name: 'Lovely Necklace',
+    image: '/images/necklace.png',
     description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
-    brand: 'Apple',
-    category: 'Electronics',
-    price: 599.99,
+      'Hand crafted by Claire the necklace master.  Perfeclty sized to fit your skinny neck.  I eat necklaces for breakfast',
+    brand: 'Beachy Bums',
+    category: 'Jewelry',
+    price: 39.99,
     countInStock: 7,
     rating: 4.0,
     numReviews: 8,
   },
   {
    
-    name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
+    name: 'Bracelet',
+    image: '/images/bracelet.png',
     description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
-    brand: 'Cannon',
-    category: 'Electronics',
-    price: 929.99,
+      'For your toes it wraps around',
+    brand: 'Beachy Bums',
+    category: 'Jewelry',
+    price: 9.99,
     countInStock: 5,
     rating: 3,
     numReviews: 12,
   },
   {
    
-    name: 'Sony Playstation 4 Pro White Version',
+    name: 'Sony Playstation',
     image: '/images/playstation.jpg',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
@@ -53,7 +53,7 @@ const products = [
   },
   {
    
-    name: 'Logitech G-Series Gaming Mouse',
+    name: 'Logitech G-Series',
     image: '/images/mouse.jpg',
     description:
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
@@ -66,7 +66,7 @@ const products = [
   },
   {
    
-    name: 'Amazon Echo Dot 3rd Generation',
+    name: 'Amazon Echo Dot',
     image: '/images/alexa.jpg',
     description:
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',

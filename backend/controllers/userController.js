85ede57ba494e0ee96c76cdc54f96441e113cfb9
Dano <danielaherniv@gmail.com>
2020-11-03 22:55:21 -0500

    creating auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
-import Product from '../models/userModel.js'
 
 // @desc Auth user & get token
 //@route POST /api/users/login
@@ -16,7 +16,7 @@ const user = await User.findOne({email})
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id),
     })
   } else {
     res.status(401)
@@ -24,6 +24,27 @@ const user = await User.findOne({email})
   }
 })
 
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+  res.send('Success')
+})
+
+
 export {
-  authUser
+  authUser, getUserProfile
 }

63f4a80fcc8debbb22fd5d12e748b78781638def
Dano <danielaherniv@gmail.com>
2020-11-14 22:43:48 -0500

    custom page titles & meta

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -12,9 +12,9 @@ const Meta = ({ title, description, keywords }) => {
 }
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Welcome To Claire\'s Site',
+  description: 'We sell the best jewlery around.',
+  keywords: 'jewlery, beads, necklaces',
 }
 
 export default Meta

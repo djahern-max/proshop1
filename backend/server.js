ffad24addaf816caa17e291881e0544964c53c67
Dano <danielaherniv@gmail.com>
2020-11-14 23:20:51 -0500

    prepare for deployemnt

--- backend/server.js
+++ backend/server.js
@@ -34,15 +34,14 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-}else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  }
-  )
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
 }
 
 
